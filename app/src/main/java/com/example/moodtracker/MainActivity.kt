c52847f5429d2507839cb22776cd298a967423d5
volkova-lisa <liza.volk999@gmail.com>
2021-12-06 13:55:26 +0200

    Revert "icons + font + splash screen ui"
    This reverts commit 41734844ea781b7c49c999c5dce2ebb9c1940461.

--- app/src/main/java/com/example/moodtracker/MainActivity.kt
+++ app/src/main/java/com/example/moodtracker/MainActivity.kt
@@ -2,16 +2,10 @@ package com.example.moodtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.moodtracker.databinding.ActivityMainBinding
-import com.example.moodtracker.databinding.FragmentSplashScreenBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private var _binding : ActivityMainBinding? = null
-    private val mBinding get() = _binding!!
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
     }
 }
