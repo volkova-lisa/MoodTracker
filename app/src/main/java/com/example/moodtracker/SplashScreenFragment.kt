41734844ea781b7c49c999c5dce2ebb9c1940461
volkova-lisa <liza.volk999@gmail.com>
2021-12-06 11:51:07 +0200

    icons + font + splash screen ui

---
+++ app/src/main/java/com/example/moodtracker/SplashScreenFragment.kt
@@ -0,0 +1,23 @@
+package com.example.moodtracker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.moodtracker.databinding.FragmentSplashScreenBinding
+
+class SplashScreenFragment : Fragment() {
+
+    private var _binding : FragmentSplashScreenBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSplashScreenBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+}
